8b60594f4c666b018b4c36ac649e848d50d13277
coder-kuntal <142230879+coder-kuntal@users.noreply.github.com>
2023-08-14 18:31:41 +0530

    Add files via upload

---
+++ Chap_03_Practice_Set.java
@@ -0,0 +1,26 @@
+public class Chap_03_Practice_Set {
+    public static void main(String[] args) {
+     //String name = "KUNTAL BERA";
+
+        // prob: 01
+     //System.out.println(name.toLowerCase());
+
+        // prob: 02
+        //System.out.println(name.replace(" ", "_"));
+
+        // prob: 03
+        //String letter = "Dear <|name|>, Thanks a lot ";
+        //letter = letter.replace("<|name|>","Kuntal");
+        //System.out.println(letter);
+
+        //prob: 04
+        //String myString = "This string contains  double and   triple spaces";
+        //System.out.println(myString.indexOf("  "));
+        //System.out.println(myString.indexOf("   "));
+
+        //prob: 05
+        //String letter2 = "Dear Kuntal,\n\t This java course is Nice.\n\t\t\t\t\t\t\t Thanks!";
+        //System.out.println(letter2);
+
+    }
+}
